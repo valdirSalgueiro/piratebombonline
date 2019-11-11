5d65ecb99e4df36db9c4376f96229922f34f7737
Valdir Salgueiro <valdir.salgueiro@arizona.global>
2019-11-11 14:55:51 -0300

    initial commit

---
+++ Tile-Sets (64-64).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="2019.10.30" name="Tile-Sets (64-64)" tilewidth="64" tileheight="64" tilecount="30" columns="6">
+ <image source="Pirate Bomb/Sprites/8-Tile-Sets/Tile-Sets (64-64).png" width="384" height="320"/>
+</tileset>
---
+++ variations.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="2019.10.30" name="1-sheet" tilewidth="64" tileheight="64" tilecount="8" columns="1">
+ <image source="Pirate Bomb/Sprites/8-Tile-Sets/Variations/1-sheet.png" width="64" height="512"/>
+</tileset>
