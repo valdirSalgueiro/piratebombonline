47242ce9c7d03eda3124364789dfd90c2135c447
Valdir Salgueiro <valdir.salgueiro@arizona.global>
2019-11-17 08:57:30 -0300

    facebook integration

---
+++ userRoutes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+
+// router.get('/', function (req, res) {	
+//     res.sendFile(__dirname + '/public/game.html');	
+//   });
+router.get("/auth", passport.authenticate("facebook"));
+router.get("/auth/facebook/callback", passport.authenticate("facebook", {
+    successRedirect: "/success",
+    failureRedirect: "/fail"
+})
+);
+router.get("/fail", (req, res) => {
+    res.send("Failed attempt");
+});
+
+router.get("/success", (req, res) => {
+    res.sendFile(__dirname + '/public/game.html');
+});
+
+
+
+module.exports = router;	
